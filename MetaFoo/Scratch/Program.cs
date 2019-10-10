d9391420e4a67cdc558b403806ed5da2e924d769
Philip Laureano <philip.laureano@gmail.com>
2019-10-11 06:56:44 +0800

    Added the scratch project

---
+++ MetaFoo/Scratch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Add the prototype code here
+        }
+    }
+}
